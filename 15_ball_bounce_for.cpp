55207834ca93809b36c7fe3b8761de436a08c5eb
Prachi Khandelwal <109075683+prachik2407@users.noreply.github.com>
2025-08-28 19:59:38 +0530

    Add files via upload

---
+++ 15_ball_bounce_for.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+int main(){
+    for(int i = 0; i<4; i++){
+        for(int j = 0; j<i; j++){
+            cout<<" ";
+        }
+        cout<<"O";
+        Sleep(1000);
+        cout<<endl;
+    }
+
+    for(int i = 0; i<=3; i++){
+        for(int j = 3; j>=i; j--){
+            cout<<" ";
+        }
+        cout<<"O";
+        Sleep(1000);
+        cout<<endl;
+    }
+
+    return 0;
+}
